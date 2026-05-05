194a074bd45b72f5346150097d789191635b7b44
Rustam <hasan83842@gmail.com>
2026-05-05 19:48:54 +0300

    Updates

--- assets/firebase-oTZ4GB3a.js
+++
@@ -1,1 +0,0 @@
-import{a as e,n as t,r as n}from"./firebase-BNno0Y_2.js";export{t as db,n as getPlayerRef,e as getRoom};
---
+++ assets/firebase-vNK-lUbt.js
@@ -0,0 +1,1 @@
+import{a as e,n as t,r as n}from"./firebase-DdjZy-9u.js";export{t as getDb,n as getPlayerRef,e as getRoom};
